530b131717ddfd96ea6431154ae2d5d2bbb68972
Cronos <jorge.escamilla89@gmail.com>
2021-04-03 01:04:55 -0600

    Change plugin name

--- src/get-colors.js
+++ src/get-colors.js
@@ -4,13 +4,15 @@ export default function() {
   const doc = sketch.getSelectedDocument()
   const colors = doc.swatches
 
+  // Get color names from variables
   const colorsNames = colors.map(color => {
     return color.name
   })
 
-  if (colors.length === 0) {
-    sketch.UI.alert('No Colors', 'There is no colors!')
-  } else {
+  // If color variables exist, show an alert with the color names
+  if (colors.length !== 0) {
     sketch.UI.alert('Color Variables', `${colorsNames}`)
+  } else {
+    sketch.UI.alert('No Colors', 'There is no colors!')
   }
 }
