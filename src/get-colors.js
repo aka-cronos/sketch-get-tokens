0b782116029e7decdfeb72c4b1469fb84056534e
Alejandro Casillas <ealejandrocasillas@gmail.com>
2021-04-19 12:02:29 -0600

    feat(plugin): get colors plugin finished (#1)

--- src/get-colors.js
+++ src/get-colors.js
@@ -1,20 +1,39 @@
-import sketch from 'sketch'
+import sketch from "sketch";
+import values from "./lib/values";
 
-export default function() {
-  const doc = sketch.getSelectedDocument()
-  const colors = doc.swatches
+export default () => {
+  const doc = sketch.getSelectedDocument();
+  const colors = doc.swatches;
 
-  // Get color names from variables
-  const colorsNames = colors.map(color => {
-    return color.name.toLowerCase()
-  })
-
-  console.log(colorsNames)
-  
-  // If color variables exist, show an alert with the color names
-  if (colors.length !== 0) {
-    sketch.UI.alert('Color Variables', `${colorsNames}`)
-  } else {
-    sketch.UI.alert('No Colors', 'There is no colors!')
+  if (!colors.length) {
+    sketch.UI.alert("No Colors", "There is no colors!");
+    return;
   }
-}
+
+  const formattedColors = colors.reduce(
+    (prev, color) => ({
+      ...prev,
+      [color.name.replace("Colors/", "").toLowerCase()]: color.color,
+    }),
+    {}
+  );
+
+  let type = "JSON";
+  let savePanel = NSSavePanel.savePanel();
+
+  savePanel.setNameFieldStringValue("colors." + values[type].filetype);
+  savePanel.setPrompt("Save");
+
+  savePanel.runModal();
+
+  let file = NSString.stringWithString(JSON.stringify(formattedColors));
+  let file_path = savePanel.URL().path();
+  file.writeToFile_atomically_encoding_error(
+    file_path,
+    true,
+    NSUTF8StringEncoding,
+    null
+  );
+
+  sketch.UI.alert("Colors exported", "Colors exporte succesfully!");
+};
