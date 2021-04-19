4e45bd28c5aee821f425ff2a8b26b31fbd95fc5a
Cronos <jorge.escamilla89@gmail.com>
2021-04-19 17:08:59 -0500

    Add prittier config

--- src/get-colors.js
+++ src/get-colors.js
@@ -1,39 +1,39 @@
-import sketch from "sketch";
-import values from "./lib/values";
+import sketch from 'sketch'
+import values from './lib/values'
 
 export default () => {
-  const doc = sketch.getSelectedDocument();
-  const colors = doc.swatches;
+  const doc = sketch.getSelectedDocument()
+  const colors = doc.swatches
 
   if (!colors.length) {
-    sketch.UI.alert("No Colors", "There is no colors!");
-    return;
+    sketch.UI.alert('No Colors', 'There is no colors!')
+    return
   }
 
   const formattedColors = colors.reduce(
     (prev, color) => ({
       ...prev,
-      [color.name.replace("Colors/", "").toLowerCase()]: color.color,
+      [color.name.replace('Colors/', '').toLowerCase()]: color.color,
     }),
     {}
-  );
+  )
 
-  let type = "JSON";
-  let savePanel = NSSavePanel.savePanel();
+  let type = 'JSON'
+  let savePanel = NSSavePanel.savePanel()
 
-  savePanel.setNameFieldStringValue("colors." + values[type].filetype);
-  savePanel.setPrompt("Save");
+  savePanel.setNameFieldStringValue('colors.' + values[type].filetype)
+  savePanel.setPrompt('Save')
 
-  savePanel.runModal();
+  savePanel.runModal()
 
-  let file = NSString.stringWithString(JSON.stringify(formattedColors));
-  let file_path = savePanel.URL().path();
+  let file = NSString.stringWithString(JSON.stringify(formattedColors))
+  let file_path = savePanel.URL().path()
   file.writeToFile_atomically_encoding_error(
     file_path,
     true,
     NSUTF8StringEncoding,
     null
-  );
+  )
 
-  sketch.UI.alert("Colors exported", "Colors exporte succesfully!");
-};
+  sketch.UI.alert('Colors exported', 'Colors exporte succesfully!')
+}
