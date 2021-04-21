c05b64d1ceea0e1ba675d387938b7321e985b636
Jorge Escamilla <jorge.escamilla89@gmail.com>
2021-04-21 17:25:21 -0500

    Create Color tokens file
    * Add prittier config
    
    * Remove values and lodash
    
    * JSON Pritty format
    
    * Fix missing import

--- src/get-colors.js
+++ src/get-colors.js
@@ -1,39 +1,39 @@
-import sketch from "sketch";
-import values from "./lib/values";
+import sketch from 'sketch'
 
 export default () => {
-  const doc = sketch.getSelectedDocument();
-  const colors = doc.swatches;
+  const doc = sketch.getSelectedDocument()
+  const colors = doc.swatches
 
   if (!colors.length) {
-    sketch.UI.alert("No Colors", "There is no colors!");
-    return;
+    sketch.UI.alert('No Colors', 'There is no colors!')
+  } else {
+    const formattedColors = colors.reduce(
+      (prev, color) => ({
+        ...prev,
+        [color.name.toLowerCase()]: color.color,
+      }),
+      {}
+    )
+
+    let type = 'json'
+    let savePanel = NSSavePanel.savePanel()
+
+    savePanel.setNameFieldStringValue('colors.' + type)
+    savePanel.setPrompt('Save')
+
+    savePanel.runModal()
+
+    let file = NSString.stringWithString(
+      JSON.stringify(formattedColors, null, 2)
+    )
+    let file_path = savePanel.URL().path()
+    file.writeToFile_atomically_encoding_error(
+      file_path,
+      true,
+      NSUTF8StringEncoding,
+      null
+    )
+
+    sketch.UI.alert('Colors exported', 'Colors exporte succesfully!')
   }
-
-  const formattedColors = colors.reduce(
-    (prev, color) => ({
-      ...prev,
-      [color.name.replace("Colors/", "").toLowerCase()]: color.color,
-    }),
-    {}
-  );
-
-  let type = "JSON";
-  let savePanel = NSSavePanel.savePanel();
-
-  savePanel.setNameFieldStringValue("colors." + values[type].filetype);
-  savePanel.setPrompt("Save");
-
-  savePanel.runModal();
-
-  let file = NSString.stringWithString(JSON.stringify(formattedColors));
-  let file_path = savePanel.URL().path();
-  file.writeToFile_atomically_encoding_error(
-    file_path,
-    true,
-    NSUTF8StringEncoding,
-    null
-  );
-
-  sketch.UI.alert("Colors exported", "Colors exporte succesfully!");
-};
+}
